f98bca6843dd4a85a571d6af4ce557bd7119c56b
sentiment-analysi <129092803+sentiment-analysi@users.noreply.github.com>
2023-05-07 11:55:21 +0530

    Update annapp1.py

--- annapp1.py
+++ annapp1.py
@@ -274,14 +274,19 @@ def run_sentiment_app():
 
                   # Show dropdown to select a USN
                   selected_usn = st.selectbox('Select a USN:', options=usns)
+
+                  # Delete the selected review
                   if selected_usn:
-                    # Show delete button
-                    delete_button = st.button('Delete')
-                    if delete_button:
-                        c.execute("DELETE FROM reviews2 WHERE usn=?", (selected_usn,))
-                        conn.commit()
-                        c.execute("VACUUM")
-                        st.success(f"Review for {selected_usn} has been deleted.")
+                      delete_button = st.button('Delete')
+
+                      if delete_button:
+                          c.execute("DELETE FROM reviews2 WHERE usn=?", (selected_usn,))
+                          conn.commit()
+                          c.execute("VACUUM")  # This optimizes the database
+                          st.success(f'Review for {selected_usn} has been deleted.')
+                  else:
+                      st.warning('Please select a USN to delete.')
+
 
                 
 
